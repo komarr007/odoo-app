46980dd39f47128a627db1eb30e6ad98518bae94
komarr007 <62511453+komarr007@users.noreply.github.com>
2022-04-04 19:13:19 +0700

    nambahin due days

--- task_latihan/models/sale_work_order.py
+++ task_latihan/models/sale_work_order.py
@@ -12,9 +12,17 @@ class WorkOrder(models.Model):
     planned_start = fields.Datetime(string="Planned Start", default=False, required=True, readonly=False)
     planned_end = fields.Datetime(string="Planned End", default=False, required=True, readonly=False)
     sale_order_id = fields.Many2one('sale.order', string="Booking Order Ref", default=False, required=True, readonly=False)
-    # date_start = fields.Datetime(string="Date Start", default=False, required=True, readonly=True)
-    # date_end = fields.Datetime(string="Date End", default=False, required=False, readonly=True)
+    due_date = fields.Char(compute="_due_date" ,string="Due Date", required=False, readonly=True)
     state = fields.Selection(
         [('pending', 'Pending'), ('in_progress', 'In_Progress'),
          ('done', 'Done'), ('cancel', 'Cancel')], string="Status", default='pending', readonly=True)
     notes = fields.Text(string="Notes", default=False, required=False, readonly=False)
+
+    @api.depends('planned_start', 'planned_end')
+    def _due_date(self):
+        for rec in self:
+            if rec.planned_start and rec.planned_end:
+                days_left = rec.planned_end - rec.planned_start
+                rec.due_date = str(days_left)
+            else:
+                rec.due_date = "try to input planned start and end"
